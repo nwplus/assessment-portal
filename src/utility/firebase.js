7079ef96b8fdec0e8c6bbf588390f8cda47cb4f6
Giulio Rossi <ciuffi9@mac.com>
2020-12-16 20:32:34 -0800

    update variable names and filter non submitted app

--- src/utility/firebase.js
+++ src/utility/firebase.js
@@ -4,14 +4,14 @@ import 'firebase/storage'
 import 'firebase/firestore'
 
 const config = {
-  apiKey: process.env.REACT_APP_DEV_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_DEV_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DEV_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_DEV_PUBLIC_FIREBASE_PROJECT_ID,
-  measurementId: process.env.REACT_APP_DEV_PUBLIC_FIREBASE_MEASUREMENT_ID,
-  appId: process.env.REACT_APP_DEV_PUBLIC_FIREBASE_APP_ID,
-  storageBucket: process.env.REACT_APP_DEV_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_DEV_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
 }
 
 const app = firebase.initializeApp(config)
@@ -21,16 +21,14 @@ export const db = firebase.firestore()
 export const storage = firebase.storage()
 
 export const getAllApplicants = async (website, callback) => {
-  return (
-    db
-      .collection('Hackathons')
-      .doc(website) // hardcode for event
-      .collection('Applicants')
-      //.where('submission.submitted', '==', true)
-      .onSnapshot(snap => {
-        callback(snap.docs.map(doc => doc.data()))
-      })
-  )
+  return db
+    .collection('Hackathons')
+    .doc(website) // hardcode for event
+    .collection('Applicants')
+    .where('submission.submitted', '==', true)
+    .onSnapshot(snap => {
+      callback(snap.docs.map(doc => doc.data()))
+    })
 }
 
 function calculateTotalScore(hackerScore) {
