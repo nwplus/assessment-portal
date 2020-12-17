e18e32d7d010b59858c8f6f036851a0a2eee5dcb
Giulio Rossi <Ciuffi9@mac.com>
2020-12-16 18:02:04 -0800

    Style the response area a little nicer

--- src/components/responseInput.js
+++ src/components/responseInput.js
@@ -1,16 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const TextArea = styled.div`
+  border: 1px solid grey;
+  padding: 15px;
+`
 
 export default function ResponseInput({ label, response }) {
   return (
     <div>
       <h5> {label} </h5>
-      <textarea
-        disabled={true}
-        readOnly={true}
-        style={{ resize: 'none' }}
-        cols={50}
-        defaultValue={response}
-      />
+      <TextArea style={{ resize: 'none', color: 'black', verticalAlign: 'middle' }}>
+        {response}
+      </TextArea>
     </div>
   )
 }
