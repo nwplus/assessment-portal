3997e3c7c4e59d898df66d4a4232dd3cd061989b
Giulio Rossi <ciuffi9@mac.com>
2020-12-18 17:45:07 -0800

    replace school and major

--- src/components/applicantResponse.js
+++ src/components/applicantResponse.js
@@ -75,8 +75,8 @@ export default function ApplicantResponse(props) {
           label="19 or over?"
           response={hacker.basicInfo?.isOfLegalAge ? 'yes' : 'no'}
         />
-        <ResponseInput label="School" response={`Studying ${hacker.basicInfo?.major}`} />
-        <ResponseInput label="Major" response={hacker.basicInfo?.school} />
+        <ResponseInput label="School" response={`Studying ${hacker.basicInfo?.school}`} />
+        <ResponseInput label="Major" response={hacker.basicInfo?.major} />
         <ResponseInput label="Visiting From" response={hacker.basicInfo?.location} />
         <ResponseInput label="Hackathons Attended" response={hacker.basicInfo.hackathonsAttended} />
         <ResponseInput
