ffa52daad79bc392507f69fa097ae072dbf10fc8
Giulio Rossi <Ciuffi9@mac.com>
2021-01-18 16:43:22 -0800

    refactor CSV button to have loading icon

--- src/components/CSVButton.js
+++ src/components/CSVButton.js
@@ -2,22 +2,35 @@ import React, { useState, useRef } from 'react'
 import Button from '../components/Button'
 import { getCSVData } from '../utility/firebase'
 import { CSVLink } from 'react-csv'
+import Spinner from '../assets/spinner.svg'
 
 export default function CSVButton() {
   const [csvData, setCSVData] = useState('')
   const downloadLink = useRef()
+  const [loading, setLoading] = useState(false)
   return (
     <>
       <Button
         width="large"
         bColor="black"
         onClick={async () => {
+          setLoading(true)
           const data = await getCSVData()
+          setLoading(false)
           setCSVData(data)
           downloadLink.current.link.click()
         }}
       >
-        Download CSV
+        <div style={{ position: 'relative', textAlign: 'center' }}>
+          Download CSV{' '}
+          {loading && (
+            <img
+              src={Spinner}
+              style={{ position: 'absolute', width: '30px', height: '30px', right: '10px' }}
+              alt="loading"
+            />
+          )}
+        </div>
       </Button>
       <CSVLink
         style={{ visibility: 'hidden' }}
