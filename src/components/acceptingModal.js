6a385cd6c8df6dae7cd28f75acc51b669f7cb159
Giulio Rossi <Ciuffi9@mac.com>
2021-01-18 16:42:44 -0800

    abstract modal

--- src/components/acceptingModal.js
+++ src/components/acceptingModal.js
@@ -1,25 +1,10 @@
-import React, { useRef, useState } from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { APPLICATION_STATUS, COLOR } from '../constants'
 import { getApplicantsToAccept, updateApplicantStatus } from '../utility/firebase'
 import Button from './Button'
-const GreyDiv = styled.div`
-  height: 100vh;
-  width: 100vw;
-  background: rgba(0, 0, 0, 0.4);
-  position: absolute;
-`
+import Modal from './Modal'
 
-const ModalDiv = styled.div`
-  width: 400px;
-  height: 200px;
-  position: absolute;
-  left: 50%;
-  top: 25%;
-  background-color: white;
-  transform: translate(-50%, -25%);
-  opacity: 100%;
-`
 const ScoreInput = styled.input`
   font-size: 16px;
   border: 1px solid ${COLOR.BLACK};
@@ -45,7 +30,6 @@ const acceptApplicant = async applicant => {
 }
 
 export default function AcceptingModal({ setShowing }) {
-  const backgroundRef = useRef()
   const [totalApplicants, setTotalApplicants] = useState(0)
   const [score, setScore] = useState(undefined)
   const [applicantsToAccept, setApplicants] = useState([])
@@ -63,50 +47,41 @@ export default function AcceptingModal({ setShowing }) {
   }
 
   return (
-    <GreyDiv
-      ref={backgroundRef}
-      onClick={e => {
-        if (backgroundRef.current && backgroundRef.current === e.target) {
-          setShowing(false)
-        }
-      }}
-    >
-      <ModalDiv>
-        <h3>Accept applicants</h3>
-        {/* <ScoreInput /> */}
-        <FlexDiv>
-          <ScoreInput
-            onChange={e => {
-              if (!isNaN(e.target.value)) setScore(e.target.value)
-            }}
-            value={score ?? ''}
-            placeholder="minimum score"
-          />
-          <TotalApplicantsP>Total applicants: {totalApplicants}</TotalApplicantsP>
-        </FlexDiv>
-        <FlexDiv style={{ justifyContent: 'center' }}>
-          <Button
-            onClick={() => {
-              getApplicants()
-            }}
-            disabled={!(score > 0)}
-            width="flex"
-            bColor={COLOR.BLUE_TEXT}
-          >
-            See count
-          </Button>
-          <Button
-            onClick={() => {
-              acceptApplicants()
-            }}
-            disabled={!(totalApplicants > 0)}
-            width="flex"
-            bColor={'green'}
-          >
-            Accept Applicants
-          </Button>
-        </FlexDiv>
-      </ModalDiv>
-    </GreyDiv>
+    <Modal setShowing={setShowing}>
+      <h3>Accept applicants</h3>
+      {/* <ScoreInput /> */}
+      <FlexDiv>
+        <ScoreInput
+          onChange={e => {
+            if (!isNaN(e.target.value)) setScore(e.target.value)
+          }}
+          value={score ?? ''}
+          placeholder="minimum score"
+        />
+        <TotalApplicantsP>Total applicants: {totalApplicants}</TotalApplicantsP>
+      </FlexDiv>
+      <FlexDiv style={{ justifyContent: 'center' }}>
+        <Button
+          onClick={() => {
+            getApplicants()
+          }}
+          disabled={!(score > 0)}
+          width="flex"
+          bColor={COLOR.BLUE_TEXT}
+        >
+          See count
+        </Button>
+        <Button
+          onClick={() => {
+            acceptApplicants()
+          }}
+          disabled={!(totalApplicants > 0)}
+          width="flex"
+          bColor={'green'}
+        >
+          Accept Applicants
+        </Button>
+      </FlexDiv>
+    </Modal>
   )
 }
